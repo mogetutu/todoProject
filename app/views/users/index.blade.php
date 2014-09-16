f4b9c6c9d3a5ec0fbc3bc6807c5d859a275017b2
loisenjoki <loisenjoki6@gmail.com>
2014-09-16 12:49:00 +0300

    new file

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -1,10 +1,7 @@
 @extends('layouts.master')
 @section('content')
-<p>this is a todo aplications that helps you to create a new to do save it on the
-    database.one can delelte edit  the tasks.
-    it also allows user to login and also regester if they are new.</p>
-    {{Form::open(route,array)}}
-    {{Form::submit('Login', ['class' => 'btn btn-success'])}}
-	{{Form::submit('Register', ['class' => 'btn btn-success'])}}
-	{{Form::close()}}
-@stop
+<div class="btn-group">
+	<a href="{{route('users.create')}}" class="btn btn-default">Register</a>
+		 <a href="{{route('users.login')}}" class="btn btn-default">Login</a> 
+		</div>
+@stops
